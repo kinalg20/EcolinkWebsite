1b0f9b97ff28d9f57799be8aa1caf31616526e5f
jpjangid <developerjp2000@gmail.com>
2022-04-26 02:41:38 -0200

    changes

--- src/app/shared/bulk-pricing/bulk-pricing.component.ts
+++ src/app/shared/bulk-pricing/bulk-pricing.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ApiServiceService } from 'src/app/Services/api-service.service';
 
 @Component({
   selector: 'app-bulk-pricing',
@@ -7,9 +9,89 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BulkPricingComponent implements OnInit {
 
-  constructor() { }
+  userObj: any;
+  invalidUserEmail: string = '';
+  resSignupMsg: string = '';
+  invalidMobile = false;
+  invalidEmail: boolean = false;
+  constructor(private __apiservice: ApiServiceService) { }
 
   ngOnInit(): void {
   }
 
+  saveBulkFormDetail(form: NgForm) {
+    if (form.valid) {
+      console.log(form.value);
+      this.userObj = {
+        first_name: form.value.firstname,
+        last_name: form.value.lastname,
+        email: form.value.email,
+        type: "bulkpricing",
+        mobile: form.value.phone,
+        address_1: form.value.address,
+        country: form.value.country,
+        state: form.value.state,
+        city: form.value.city,
+        zip: form.value.pincode,
+        input_1 : form.value.input_11,
+        input_2: form.value.help
+      };
+      this.__apiservice.submitFormDetail(this.userObj).subscribe((res: any) => {
+        console.log(res);
+        form.reset();
+      }
+      )
+    }
+    else {
+      this.resSignupMsg = 'Please fill the value';
+    }
+  }
+
+  validateUserEmail(email: any) {
+    const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (re.test(email.target.value) == false) {
+      this.invalidUserEmail = 'Invalid Email Address';
+      return false;
+    }
+    this.invalidUserEmail = '';
+    return true;
+  }
+  validateEmail(event: any) {
+    const value = event.target.value;
+
+    if (
+      value &&
+      !/^[0-9]*$/.test(value) &&
+      !this.validateUserEmail(event)
+    ) {
+      this.invalidEmail = true;
+    }
+
+    else {
+      this.invalidEmail = false;
+    }
+  }
+  inputMobile(event: any) {
+    if (
+      event.key.length === 1 &&
+      !/^[0-9]$/.test(event.key)
+    ) {
+      event.preventDefault();
+    }
+  }
+  validateMobile(event: any) {
+    const value = event.target.value;
+
+    if (
+      value &&
+      /^[0-9]+$/.test(value) &&
+      value.length < 10
+    ) {
+      this.invalidMobile = true;
+    }
+
+    else {
+      this.invalidMobile = false;
+    }
+  }
 }
