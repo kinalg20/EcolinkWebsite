5242ca74adde4f4539f15da8397af146e25a6afc
jpjangid <developerjp2000@gmail.com>
2022-05-12 09:54:11 -0200

    commit

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -10,37 +10,13 @@ export class AuthGuard implements CanActivate {
   email_token: any;
   constructor(private router: Router) { }
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    if (localStorage.getItem('ecolink_user_credential')) {
-      this.user_authenticated = localStorage.getItem('ecolink_user_credential');
-      console.log(JSON.parse(this.user_authenticated).user.remember_token);
-      if (JSON.parse(this.user_authenticated).user.remember_token == null) {
-        this.router.navigateByUrl("/profile");
-        return true;
-      }
-
-      else if (localStorage.getItem('email_token')) {
-        this.email_token = localStorage.getItem('email_token');
-        if (JSON.parse(this.user_authenticated).user.remember_token == JSON.parse(this.email_token)) {
-          return true;
-        }
-        else {
-          this.router.navigateByUrl("/profile/auth");
-          return false;
-        }
-      }
-
-      else {
-        this.router.navigateByUrl("/profile/auth");
-        // localStorage.removeItem('ecolink_user_credential');
-        return false;
-      }
-
-
+    if(localStorage.getItem('ecolink_user_credential')){
+      return true;
     }
-    else {
-      this.router.navigateByUrl("/profile/auth");
+    else{
       return false;
     }
   }
+ 
 
 }
