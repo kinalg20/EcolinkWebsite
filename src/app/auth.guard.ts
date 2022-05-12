722c14afbd9788a022cff68097763f44dc26adcc
kinalg20 <kinal27213@gmail.com>
2022-05-12 13:00:08 +0530

    commit

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -6,36 +6,19 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
-  user_authenticated: any;
-  email_token: any;
   constructor(private router: Router) { }
+  user_detail: any;
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     if (localStorage.getItem('ecolink_user_credential')) {
-      this.user_authenticated = localStorage.getItem('ecolink_user_credential');
-      console.log(JSON.parse(this.user_authenticated).user.remember_token);
-      if (JSON.parse(this.user_authenticated).user.remember_token == null) {
-        this.router.navigateByUrl("/profile");
+      this.user_detail = localStorage.getItem('ecolink_user_credential');
+      let detail = JSON.parse(this.user_detail);
+      if (detail.user.email_verified) {
         return true;
       }
-
-      else if (localStorage.getItem('email_token')) {
-        this.email_token = localStorage.getItem('email_token');
-        if (JSON.parse(this.user_authenticated).user.remember_token == JSON.parse(this.email_token)) {
-          return true;
-        }
-        else {
-          this.router.navigateByUrl("/profile/auth");
-          return false;
-        }
-      }
-
       else {
         this.router.navigateByUrl("/profile/auth");
-        // localStorage.removeItem('ecolink_user_credential');
         return false;
       }
-
-
     }
     else {
       this.router.navigateByUrl("/profile/auth");
