a206abb0663a3775fab927aa13bd7aac44c7ab37
kinalg20 <kinal27213@gmail.com>
2022-05-12 13:08:22 +0530

    commit

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRoute, ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -8,13 +8,20 @@ import { Observable } from 'rxjs';
 export class AuthGuard implements CanActivate {
   constructor(private router: Router) { }
   user_detail: any;
+  email_token: any;
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     if (localStorage.getItem('ecolink_user_credential')) {
+      this.email_token = localStorage.getItem('email_token');
       this.user_detail = localStorage.getItem('ecolink_user_credential');
       let detail = JSON.parse(this.user_detail);
       if (detail.user.email_verified) {
         return true;
       }
+
+      else if(this.email_token == this.user_detail.user.remember_token){
+        return true;
+      }
+
       else {
         this.router.navigateByUrl("/profile/auth");
         return false;
