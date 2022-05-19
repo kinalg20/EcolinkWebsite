4b6199935cc55adc189142591b274b3830b896d0
jpjangid <developerjp2000@gmail.com>
2022-05-19 10:42:34 -0200

    changes

--- src/app/shared/media-banner/media-banner.component.ts
+++ src/app/shared/media-banner/media-banner.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-media-banner',
@@ -12,20 +12,45 @@ export class MediaBannerComponent implements OnInit {
   blogArray:any=[];
   helperArray:any=[]
   count:any=0;
+  elements:any = [];
   constructor() { }
-
+  @HostListener("window:scroll", [])
+  onScroll() {
+    if (this.bottomReached()) {
+      this.count=0
+      this.data_input.map((res:any,index:any)=> {
+        if(this.count<3){
+          this.helperArray.push(res);
+          this.blogArray.push(res);
+          this.data_input.splice(index,1);
+          this.count++;
+        }
+      })
+      
+      // this.elements = [...this.elements, this.count++];
+    }
+  }
+  bottomReached(): boolean {
+    return (window.innerHeight + window.scrollY) >= document.body.offsetHeight;
+  }
   ngOnInit(): void {
     setTimeout(() => {
       console.log(this.data_input);
-      this.data_input.map((res:any)=> {
+      this.data_input.map((res:any, index:any)=> {
         if(this.count<3){
           this.blogArray.push(res);
           this.helperArray.push(res);
+          this.data_input.splice(index,1);
           this.count++;
         }
       })
       console.log(this.blogArray);
     }, 1000);
+    // this.data_input.map((res:any)=> {
+    //   this.elements.push(res);
+    //   console.log(this.elements)
+    // })
+    
   }
   loadAllBlog() {
     this.moreCheck=!this.moreCheck;
